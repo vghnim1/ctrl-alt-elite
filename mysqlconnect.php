1588af43f4f457f79d09de8a806452d37582e928
SebasL894 <sl894@njit.edu>
2025-10-20 20:51:52 -0400

    Add mysqlconnect.php file

---
+++ mysqlconnect.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+
+$mydb = new mysqli('127.0.0.1','Sebas','IT490','authorizationdb');
+
+if ($mydb->errno != 0)
+{
+	echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+	exit(0);
+}
+
+echo "successfully connected to database".PHP_EOL;
+
+$query = "select * from users;";
+
+$response = $mydb->query($query);
+if ($mydb->errno != 0)
+{
+	echo "failed to execute query:".PHP_EOL;
+	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+	exit(0);
+}
+
+
+?>
