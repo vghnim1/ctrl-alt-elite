c50f076e74b295f557f8a4f2ee8af7668be6a7d0
vghnim1 <vg529@njit.edu>
2025-10-22 12:52:34 -0400

    adding functions file, adding start session function

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+    
+    // start a session for a user
+    function startSession($userId=null){
+        // TODO generate a session id
+        // TODO expires after 1 hour
+        // TODO secure cookie
+        // TODO return session id
+    }
+
+?>
